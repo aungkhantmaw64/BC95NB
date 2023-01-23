12d2fe579fb5267015879389d9defba81bc8b38d
Aung Khant Maw <aungkhantmaw64@gmail.com>
2023-01-23 11:36:53 +0700

    added test cases for initial MockSerial implementation

---
+++ test/test_desktop/test_MockSerial.h
@@ -0,0 +1,50 @@
+#include <ArduinoFake.h>
+#include <unity.h>
+#include "mocks/MockSerial.h"
+
+using namespace fakeit;
+
+namespace MockSerialTest
+{
+    void test_MockSerial_ClearTxBufferAfterCreate()
+    {
+        MockSerial mock_serial;
+
+        TEST_ASSERT_EQUAL_STRING("", mock_serial.getTxBuffer().c_str());
+    }
+
+    void test_MockSerial_PutsDataOnTxBufferAfterPrint(void)
+    {
+        MockSerial mock_serial;
+
+        mock_serial.begin();
+
+        Stream *stream = ArduinoFakeMock(Stream);
+
+        int totalPrintedBytes = 0;
+        totalPrintedBytes = stream->print("Data");
+
+        TEST_ASSERT_EQUAL(4, totalPrintedBytes);
+        TEST_ASSERT_EQUAL_STRING("Data", mock_serial.getTxBuffer().c_str());
+    }
+
+    void test_MockSerial_CleanTxBuffer(void)
+    {
+        MockSerial mock_serial;
+        Stream *stream = ArduinoFakeMock(Stream);
+
+        mock_serial.begin();
+
+        stream->print("Data");
+        TEST_ASSERT_EQUAL_STRING("Data", mock_serial.getTxBuffer().c_str());
+
+        mock_serial.cleanTxBuffer();
+
+        TEST_ASSERT_EQUAL_STRING("", mock_serial.getTxBuffer().c_str());
+    }
+    void run_tests(void)
+    {
+        RUN_TEST(test_MockSerial_ClearTxBufferAfterCreate);
+        RUN_TEST(test_MockSerial_PutsDataOnTxBufferAfterPrint);
+    }
+}
