4c9b1fbde059f7bd69940db861facf9955ce5df7
Aung Khant Maw <aungkhantmaw64@gmail.com>
2023-02-24 15:25:12 +0700

    [refactor] seperated implementation and interfaces

--- test/test_desktop/mocks/MockSerial.h
+++ test/test_desktop/mocks/MockSerial.h
@@ -10,67 +10,79 @@ using namespace fakeit;
 class MockSerial
 {
 public:
-    MockSerial() : _singleMode(true)
-    {
-
-        _txBuffer.reserve(100);
-        _rxBuffer.reserve(100);
-    }
-
-    ~MockSerial()
-    {
-        _txBuffer = "";
-        _rxBuffer = "";
-    }
-    void begin(void)
-    {
-        When(OverloadedMethod(ArduinoFake(Stream), print, size_t(const char *)))
-            .AlwaysDo([this](const char *str) -> int
-                      { 
-                      _txBuffer += String(str) ;
-                      return strlen(str); });
-        When(Method(ArduinoFake(Stream), read))
-            .AlwaysDo([this]() -> char
-                      { return _mockRead(); });
-        When(Method(ArduinoFake(Stream), available))
-            .AlwaysDo([this]() -> int
-                      { return _rxBuffer.length(); });
-        When(Method(ArduinoFake(Stream), flush))
-            .AlwaysReturn();
-    }
-    String getTxBuffer(void)
-    {
-        return _txBuffer;
-    }
-    void cleanTxBuffer(void)
-    {
-        _txBuffer = "";
-    }
-    String getRxContent(void)
-    {
-        return _rxBuffer;
-    }
-    void setRxContent(const char *str)
-    {
-        _rxBuffer = String(str);
-    }
-
-    char _mockRead()
-    {
-        if (_rxBuffer.length() > 0)
-        {
-            char byte = _rxBuffer.charAt(0);
-            _rxBuffer.remove(0, 1);
-            return byte;
-        }
-        return -1;
-    }
+    MockSerial();
+    ~MockSerial();
+    void begin(void);
+    String getTxBuffer(void);
+    String getRxContent(void);
+    void cleanTxBuffer(void);
+    void setRxContent(const char *str);
 
 private:
     String _txBuffer;
     String _rxBuffer;
     std::map<String, String> _cmdDict;
-    bool _singleMode;
+
+    char _mockRead(void);
 };
 
+MockSerial ::MockSerial()
+{
+    _txBuffer.reserve(100);
+    _rxBuffer.reserve(100);
+    _txBuffer = "";
+}
+
+MockSerial ::~MockSerial()
+{
+    _txBuffer = "";
+    _rxBuffer = "";
+}
+
+void MockSerial::begin()
+{
+    When(OverloadedMethod(ArduinoFake(Stream), print, size_t(const char *)))
+        .AlwaysDo([this](const char *str) -> int
+                  { 
+                      _txBuffer += String(str) ;
+                      return strlen(str); });
+    When(Method(ArduinoFake(Stream), read))
+        .AlwaysDo([this]() -> char
+                  { return _mockRead(); });
+    When(Method(ArduinoFake(Stream), available))
+        .AlwaysDo([this]() -> int
+                  { return _rxBuffer.length(); });
+    When(Method(ArduinoFake(Stream), flush))
+        .AlwaysReturn();
+}
+
+String MockSerial::getTxBuffer(void)
+{
+    return _txBuffer;
+}
+void MockSerial::cleanTxBuffer(void)
+{
+    _txBuffer = "";
+}
+
+String MockSerial::getRxContent(void)
+{
+    return _rxBuffer;
+}
+
+void MockSerial::setRxContent(const char *str)
+{
+    _rxBuffer = String(str);
+}
+
+char MockSerial::_mockRead()
+{
+    if (_rxBuffer.length() > 0)
+    {
+        char byte = _rxBuffer.charAt(0);
+        _rxBuffer.remove(0, 1);
+        return byte;
+    }
+    return -1;
+}
 #endif
