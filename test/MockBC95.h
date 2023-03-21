7989fc77c2c9506006ce540483b5c81f3a754c88
Aung Khant Maw <aungkhantmaw64@gmail.com>
2023-03-21 14:10:34 +0700

    [new] added boilerplate code for BC95 Mock

--- test/MockBC95.h
+++ test/MockBC95.h
@@ -1,101 +1,39 @@
-#ifndef D_MOCK_BC95_H
-#define D_MOCK_BC95_H
+#ifndef D_MOCKBC95_H
+#define D_MOCKBC95_H
 
-#include <BC95.h>
+#include "Modem.h"
 
-class BC95Mock : public Modem
+class MockBC95 : public Modem
 {
 public:
-    BC95Mock();
-    ~BC95Mock();
+    MockBC95(){};
+    ~MockBC95(){};
     void begin() override;
     void send(const char *cmd) override;
     void send(const String &cmd) override;
     int waitForResponse(unsigned long timeout_ms, String *buffer = NULL) override;
-    //==================Get Modem's Information=====================//
+    ;
     bool isReady() override;
-    String getManufacturerRevision() override;
-    String getIMEI() override;
-    String getICCID() override;
-    String getIMSI() override;
-    String getRSSI() override;
-
-    String getLastCmd(void);
-    void setResponseStatus(int status);
-    void setResponse(const char *resp);
-
-private:
-    String _lastCmd;
-    int _respStatus;
-    String _resp;
 };
 
-BC95Mock::BC95Mock()
-    : _lastCmd(""),
-      _respStatus(MODEM_STATUS_UNKNOWN)
-{
-    _resp.reserve(100);
-}
-
-BC95Mock::~BC95Mock()
+void MockBC95::begin()
 {
 }
 
-void BC95Mock::begin()
+void MockBC95::send(const char *cmd)
 {
 }
 
-void BC95Mock::send(const char *cmd)
+void MockBC95::send(const String &cmd)
 {
-    _lastCmd = String(cmd) + "\r";
-}
-void BC95Mock::send(const String &cmd)
-{
-}
-int BC95Mock::waitForResponse(unsigned long timeout_ms, String *buffer)
-{
-    if (buffer)
-    {
-        *buffer = _resp;
-    }
-    return _respStatus;
-}
-bool BC95Mock::isReady()
-{
-    return false;
-}
-String BC95Mock::getManufacturerRevision()
-{
-    return "";
-}
-String BC95Mock::getIMEI()
-{
-    return "";
-}
-String BC95Mock::getICCID()
-{
-    return "";
-};
-String BC95Mock::getIMSI()
-{
-    return "";
-};
-String BC95Mock::getRSSI()
-{
-    return "";
-}
-String BC95Mock::getLastCmd(void)
-{
-    return _lastCmd;
-}
-void BC95Mock::setResponseStatus(int status)
-{
-    _respStatus = status;
 }
 
-void BC95Mock::setResponse(const char *resp)
+int MockBC95::waitForResponse(unsigned long timeout_ms, String *buffer)
 {
-    _resp = String(resp);
+}
+
+bool MockBC95::isReady()
+{
 }
 
 #endif
