72769eeee1e3ff3b4276b4c10b492256e5ad3326
Aung Khant Maw <aungkhantmaw64@gmail.com>
2023-03-22 17:12:39 +0700

    fixed incorrect responses in both test and implementations of begin() and end()

--- src/BC95MQTT.cpp
+++ src/BC95MQTT.cpp
@@ -15,17 +15,17 @@ int BC95MQTT::begin(const char *host, int port)
     modem_->sendf("AT+QMTOPEN=0,\"%s\",%d", host, port);
     if (MODEM_STATUS_VALID_RESPONSE == modem_->waitForResponse(10000, &buffer_))
     {
-        if (buffer_.indexOf("+QMTOPEN:0,0") != -1)
+        if (buffer_.indexOf("+QMTOPEN: 0,0") != -1)
             return MQTT_NETWORK_OPENED;
-        if (buffer_.indexOf("+QMTOPEN:0,1") != -1)
+        if (buffer_.indexOf("+QMTOPEN: 0,1") != -1)
             return MQTT_NETWORK_WRONG_PARAMETER;
-        if (buffer_.indexOf("+QMTOPEN:0,2") != -1)
+        if (buffer_.indexOf("+QMTOPEN: 0,2") != -1)
             return MQTT_NETWORK_IDENTIFIER_OCCUPIED;
-        if (buffer_.indexOf("+QMTOPEN:0,3") != -1)
+        if (buffer_.indexOf("+QMTOPEN: 0,3") != -1)
             return MQTT_NETWORK_PDP_ACTIVATION_FAILED;
-        if (buffer_.indexOf("+QMTOPEN:0,4") != -1)
+        if (buffer_.indexOf("+QMTOPEN: 0,4") != -1)
             return MQTT_NETWORK_DNS_PARSE_FAILED;
-        if (buffer_.indexOf("+QMTOPEN:0,5") != -1)
+        if (buffer_.indexOf("+QMTOPEN: 0,5") != -1)
             return MQTT_NETWORK_DISCONNECTED;
     }
     return MQTT_NETWORK_OPEN_FAILED;
@@ -55,7 +55,7 @@ int BC95MQTT::end(void)
     modem_->send("AT+QMTCLOSE=0");
     if (MODEM_STATUS_VALID_RESPONSE == modem_->waitForResponse(300, &buffer_))
     {
-        if (buffer_.indexOf("+QMTCLOSE:0,0") != -1)
+        if (buffer_.indexOf("+QMTCLOSE: 0,0") != -1)
             return MQTT_NETWORK_CLOSED;
     }
     return MQTT_NETWORK_CLOSE_FAILED;
--- test/test_BC95MQTT/main.cpp
+++ test/test_BC95MQTT/main.cpp
@@ -10,13 +10,13 @@ BC95MQTT mqtt(&mock);
 
 void test_BC95MQTT_begin()
 {
-    mock.responseExpect("\r\nOK\r\n\r\n+QMTOPEN:0,-1\r\n", MODEM_STATUS_VALID_RESPONSE);
-    mock.responseExpect("\r\nOK\r\n\r\n+QMTOPEN:0,0\r\n", MODEM_STATUS_VALID_RESPONSE);
-    mock.responseExpect("\r\nOK\r\n\r\n+QMTOPEN:0,1\r\n", MODEM_STATUS_VALID_RESPONSE);
-    mock.responseExpect("\r\nOK\r\n\r\n+QMTOPEN:0,2\r\n", MODEM_STATUS_VALID_RESPONSE);
-    mock.responseExpect("\r\nOK\r\n\r\n+QMTOPEN:0,3\r\n", MODEM_STATUS_VALID_RESPONSE);
-    mock.responseExpect("\r\nOK\r\n\r\n+QMTOPEN:0,4\r\n", MODEM_STATUS_VALID_RESPONSE);
-    mock.responseExpect("\r\nOK\r\n\r\n+QMTOPEN:0,5\r\n", MODEM_STATUS_VALID_RESPONSE);
+    mock.responseExpect("\r\nOK\r\n\r\n+QMTOPEN: 0,-1\r\n", MODEM_STATUS_VALID_RESPONSE);
+    mock.responseExpect("\r\nOK\r\n\r\n+QMTOPEN: 0,0\r\n", MODEM_STATUS_VALID_RESPONSE);
+    mock.responseExpect("\r\nOK\r\n\r\n+QMTOPEN: 0,1\r\n", MODEM_STATUS_VALID_RESPONSE);
+    mock.responseExpect("\r\nOK\r\n\r\n+QMTOPEN: 0,2\r\n", MODEM_STATUS_VALID_RESPONSE);
+    mock.responseExpect("\r\nOK\r\n\r\n+QMTOPEN: 0,3\r\n", MODEM_STATUS_VALID_RESPONSE);
+    mock.responseExpect("\r\nOK\r\n\r\n+QMTOPEN: 0,4\r\n", MODEM_STATUS_VALID_RESPONSE);
+    mock.responseExpect("\r\nOK\r\n\r\n+QMTOPEN: 0,5\r\n", MODEM_STATUS_VALID_RESPONSE);
 
     TEST_ASSERT_EQUAL(MQTT_NETWORK_OPEN_FAILED, mqtt.begin("test.mosquitto.org", 8883));
     TEST_ASSERT_EQUAL(MQTT_NETWORK_OPENED, mqtt.begin("test.mosquitto.org", 8883));
@@ -45,8 +45,8 @@ void test_BC95MQTT_configDNSAddress(void)
 
 void test_BC95MQTT_end()
 {
-    mock.responseExpect("\r\nOK\r\n\r\n+QMTCLOSE:0,-1\r\n", MODEM_STATUS_VALID_RESPONSE);
-    mock.responseExpect("\r\nOK\r\n\r\n+QMTCLOSE:0,0\r\n", MODEM_STATUS_VALID_RESPONSE);
+    mock.responseExpect("\r\nOK\r\n\r\n+QMTCLOSE: 0,-1\r\n", MODEM_STATUS_VALID_RESPONSE);
+    mock.responseExpect("\r\nOK\r\n\r\n+QMTCLOSE: 0,0\r\n", MODEM_STATUS_VALID_RESPONSE);
     TEST_ASSERT_EQUAL(MQTT_NETWORK_CLOSE_FAILED, mqtt.end());
     TEST_ASSERT_EQUAL(MQTT_NETWORK_CLOSED, mqtt.end());
 }
