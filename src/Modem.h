afea7bec6d706b653f3645980f2b99e63dc732f0
Aung Khant Maw <aungkhantmaw64@gmail.com>
2023-03-22 09:51:42 +0700

    added docstring for the virtual class

--- src/Modem.h
+++ src/Modem.h
@@ -23,11 +23,45 @@ enum
 class Modem
 {
 public:
+    /**
+     * @brief Initializes the modem device
+     *
+     */
     virtual void begin() = 0;
+    /**
+     * @brief Sends AT command to the modem device
+     *
+     * @param cmd The AT command to be sent
+     */
     virtual void send(const char *cmd) = 0;
+    /**
+     * @brief Sends AT command to the modem device
+     *
+     * @param cmd The AT command to be sent
+     */
     virtual void send(const String &cmd) = 0;
+    /**
+     * @brief Sends AT command to the modem device.
+     *        This method allows formatted string as the input.
+     *
+     * @param fmt The formatted string.
+     * @param ...
+     */
     virtual void sendf(const char *fmt, ...) = 0;
+    /**
+     * @brief Wait for the response from the modem device until timeout occurs.
+     *
+     * @param timeout_ms The timeout duration to wait for the response.
+     * @param buffer The pointer to the buffer to store the response.
+     * @return int The status of the response
+     */
     virtual int waitForResponse(unsigned long timeout_ms, String *buffer = NULL) = 0;
+    /**
+     * @brief Checks if the modem is ready to receive the AT commands.
+     *
+     * @return true
+     * @return false
+     */
     virtual bool isReady() = 0;
 };
 
