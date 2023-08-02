7a68fa33e8166b4cd80d9d83695adde1bea7ef7f
Aung Khant Maw <aungkhantmaw64@gmail.com>
2023-08-02 17:35:31 +0700

    [feat] implement and test initial reset api

--- test/test_Modem/test_Modem.cpp
+++ test/test_Modem/test_Modem.cpp
@@ -1,7 +1,47 @@
 #include <gtest/gtest.h>
+#include <ArduinoFake.h>
 
-TEST(Modem, example){
+#define DEFAULT_RESET_PIN 4
 
+using namespace fakeit;
+
+class Modem
+{
+public:
+    Modem(uint8_t _resetPin, bool _activeHigh)
+        : m_resetPin(_resetPin),
+          m_activeHigh(_activeHigh){
+
+          };
+    void reset()
+    {
+        digitalWrite(m_resetPin, m_activeHigh);
+        delay(200);
+        digitalWrite(m_resetPin, !m_activeHigh);
+        delay(200);
+    };
+
+private:
+    uint8_t m_resetPin;
+    bool m_activeHigh;
+};
+
+TEST(Modem, RebootsDueToHardwareReset)
+{
+    When(Method(ArduinoFake(), digitalWrite)).AlwaysReturn();
+    When(Method(ArduinoFake(), delay)).AlwaysReturn();
+
+    bool givenPinModes[] = {HIGH, LOW};
+    bool mode = givenPinModes[0];
+    const uint8_t resetPin = 12;
+    bool activeHigh = mode;
+    Modem modem(resetPin, activeHigh);
+
+    modem.reset();
+
+    Verify(Method(ArduinoFake(), digitalWrite).Using(resetPin, mode)).Once();
+    Verify(Method(ArduinoFake(), digitalWrite).Using(resetPin, !mode)).Once();
+    Verify(Method(ArduinoFake(), delay).Using(200)).Exactly(2_Times);
 }
 
 int main(int argc, char **argv)
@@ -12,7 +52,7 @@ int main(int argc, char **argv)
     // ::testing::InitGoogleMock(&argc, argv);
 
     if (RUN_ALL_TESTS())
-    ;
+        ;
 
     // Always return zero-code and allow PlatformIO to parse results
     return 0;
