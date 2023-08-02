90c5dff54c3b0662944b5cdc9dd392a86cdc8825
Aung Khant Maw <aungkhantmaw64@gmail.com>
2023-08-02 15:43:43 +0700

    [feat] add initial test example to see if platformio works

---
+++ test/test_Modem/test_Modem.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+
+TEST(Modem, example){
+
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    // if you plan to use GMock, replace the line above with
+    // ::testing::InitGoogleMock(&argc, argv);
+
+    if (RUN_ALL_TESTS())
+    ;
+
+    // Always return zero-code and allow PlatformIO to parse results
+    return 0;
+}
