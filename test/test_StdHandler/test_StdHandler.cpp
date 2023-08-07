d9227a93f1563ce9dd8f8f31a730f550acae62c0
Aung Khant Maw <aungkhantmaw64@gmail.com>
2023-08-07 16:41:47 +0700

    [feat] implement and test cereg

--- test/test_StdHandler/test_StdHandler.cpp
+++ test/test_StdHandler/test_StdHandler.cpp
@@ -21,6 +21,11 @@ typedef struct
     char imsi[STD_HANDLER_IMSI_LENGTH];
 } CIMI_t;
 
+typedef struct
+{
+    int8_t n;
+    int8_t stat;
+} CEREG_t;
 class StdHandler : public ModemResponseHandler
 {
 public:
@@ -28,6 +33,7 @@ public:
     {
         m_cfun.fun = -1;
         memset(m_cimi.imsi, 0, STD_HANDLER_IMSI_LENGTH);
+        memset(&m_cereg, -1, sizeof(CEREG_t));
     }
     ~StdHandler(){};
     void onReceive(String &_response) override
@@ -36,6 +42,11 @@ public:
         {
             m_cfun.fun = _response.charAt(_response.indexOf(":") + 1) - '0';
         }
+        else if (_response.indexOf("+CEREG:") != -1)
+        {
+            m_cereg.n = _response.charAt(_response.indexOf(":") + 1) - '0';
+            m_cereg.stat = _response.charAt(_response.indexOf(",") + 1) - '0';
+        }
         else if (isdigit(_response.charAt(0)) && (_response.length() == (STD_HANDLER_IMSI_LENGTH - 1)))
         {
             memcpy(m_cimi.imsi, _response.c_str(), STD_HANDLER_IMSI_LENGTH);
@@ -43,16 +54,21 @@ public:
     }
     void getCFUN(CFUN_t *_result)
     {
-        _result->fun = m_cfun.fun;
+        *_result = m_cfun;
     }
     void getCIMI(CIMI_t *_result)
     {
         memcpy(_result->imsi, m_cimi.imsi, STD_HANDLER_IMSI_LENGTH);
     }
+    void getCEREG(CEREG_t *_result)
+    {
+        *_result = m_cereg;
+    }
 
 private:
     CFUN_t m_cfun;
     CIMI_t m_cimi;
+    CEREG_t m_cereg;
 };
 
 class StdHandlerTest : public ::testing::Test
@@ -102,6 +118,25 @@ TEST_F(StdHandlerTest, handleCIMI)
     modem->waitForResponse(1000);
     stdHandler.getCIMI(&cimi);
     EXPECT_STREQ(cimi.imsi, "460111174590523");
+}
+
+TEST_F(StdHandlerTest, handleCEREG)
+{
+    Modem *modem = modemBuilder->buildModem();
+    modem = modemBuilder->buildModem();
+    StdHandler stdHandler;
+    modem->addResponseHandler(&stdHandler);
+    testSupport.putRxBuffer("\r\n+CEREG:0,1\r\nOK\r\n");
+
+    CEREG_t cereg;
+    stdHandler.getCEREG(&cereg);
+    EXPECT_LT(cereg.n, 0);
+    EXPECT_LT(cereg.stat, 0);
+
+    modem->waitForResponse(1000);
+    stdHandler.getCEREG(&cereg);
+    EXPECT_EQ(cereg.n, 0);
+    EXPECT_EQ(cereg.stat, 1);
 }
 
 int main(int argc, char **argv)
