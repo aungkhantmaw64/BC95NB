d74dafcb7d43433c075ff9e6ec809fa2c3dbc82b
Aung Khant Maw <aungkhantmaw64@gmail.com>
2023-08-08 15:55:48 +0700

    [feat] implement and test getCEREG

--- test/test_StdModem/test_StdModem.cpp
+++ test/test_StdModem/test_StdModem.cpp
@@ -17,6 +17,11 @@ typedef struct CIMI
     char imsi[STD_MODEM_MAX_IMSI_LENGTH];
 } CIMI_t;
 
+typedef struct CEREG
+{
+    int8_t n;
+    int8_t stat;
+} CEREG_t;
 class StdModem
 {
 public:
@@ -51,6 +56,25 @@ public:
         }
     }
 
+    void getCEREG(CEREG_t *_result)
+    {
+        String response;
+        m_modem.send("AT+CEREG?\r\n");
+        ResponseCode retCode = m_modem.waitForResponse(300, &response);
+        if (retCode == ResponseCode::OK)
+        {
+            int8_t n_index = response.indexOf("+CEREG:") + strlen("+CEREG:");
+            int8_t stat_index = response.indexOf(",", n_index) + 1;
+            _result->n = response.charAt(n_index) - '0';
+            _result->stat = response.charAt(stat_index) - '0';
+        }
+        else
+        {
+            _result->n = -1;
+            _result->stat = -1;
+        }
+    }
+
 private:
     Modem &m_modem;
 };
@@ -67,10 +91,11 @@ protected:
     void TearDown() override
     {
         delete modemBuilder;
+        testSupport.reset();
     }
 };
 
-TEST_F(StdModemTest, parseCFUN)
+TEST_F(StdModemTest, getCFUN)
 {
     Modem *modem = modemBuilder->buildModem();
     StdModem stdModem(*modem);
@@ -82,7 +107,7 @@ TEST_F(StdModemTest, parseCFUN)
     EXPECT_STREQ(testSupport.getTxBuffer().c_str(), "AT+CFUN?\r\n");
 }
 
-TEST_F(StdModemTest, parseCIMI)
+TEST_F(StdModemTest, getCIMI)
 {
     Modem *modem = modemBuilder->buildModem();
     StdModem stdModem(*modem);
@@ -93,6 +118,19 @@ TEST_F(StdModemTest, parseCIMI)
     stdModem.getCIMI(&cimi);
     EXPECT_STREQ(cimi.imsi, "460111174590523");
     EXPECT_STREQ(testSupport.getTxBuffer().c_str(), "AT+CIMI\r\n");
+}
+
+TEST_F(StdModemTest, getCEREG)
+{
+    Modem *modem = modemBuilder->buildModem();
+    testSupport.putRxBuffer("\r\n+CEREG:0,1\r\nOK\r\n");
+
+    CEREG_t cereg;
+    StdModem stdModem(*modem);
+    stdModem.getCEREG(&cereg);
+    EXPECT_EQ(cereg.n, 0);
+    EXPECT_EQ(cereg.stat, 1);
+    EXPECT_STREQ(testSupport.getTxBuffer().c_str(), "AT+CEREG?\r\n");
 }
 
 int main(int argc, char **argv)
