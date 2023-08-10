b9f1a8ca1aeb887c7df7f5c977c15cea53f7b655
Aung Khant Maw <aungkhantmaw64@gmail.com>
2023-08-10 14:54:28 +0700

    [feat] add more state checking for BC95NB begin

--- test/test_BC95NB/test_BC95NB.cpp
+++ test/test_BC95NB/test_BC95NB.cpp
@@ -10,7 +10,11 @@ enum class BC95NBState
     QUERY_RF_FUNC,
     WAIT_QUERY_RF_FUNC_RESPONSE,
     QUERY_IMSI,
-    WAIT_QUERY_IMSI_RESPONSE
+    WAIT_QUERY_IMSI_RESPONSE,
+    QUERY_NET_REGISTRATION,
+    WAIT_QUERY_NET_REGISTRATION_RESPONSE,
+    QUERY_NET_ATTACHMENT,
+    WAIT_QUERY_NET_ATTACHMENT_RESPONSE
 };
 
 class BC95NB
@@ -20,6 +24,7 @@ public:
         : m_state(BC95NBState::QUERY_RF_FUNC)
     {
         m_stdModem.attach(modem);
+        memset(m_imsi, 0, STD_MODEM_MAX_IMSI_LENGTH);
     };
     ~BC95NB(){};
     BC95NBState getState()
@@ -55,8 +60,46 @@ public:
             m_state = BC95NBState::WAIT_QUERY_IMSI_RESPONSE;
             break;
         }
+        case BC95NBState::WAIT_QUERY_IMSI_RESPONSE:
+        {
+            m_stdModem.wait(300, STD_AtCmd::CIMI);
+            CIMI_t cimi;
+            m_stdModem.getCIMI(&cimi);
+            if (cimi.imsi[0] != 0)
+            {
+                strncpy(m_imsi, cimi.imsi, STD_MODEM_MAX_IMSI_LENGTH);
+                m_state = BC95NBState::QUERY_NET_REGISTRATION;
+            }
+            break;
+        }
+        case BC95NBState::QUERY_NET_REGISTRATION:
+        {
+            m_stdModem.readCmd(STD_AtCmd::CEREG);
+            m_state = BC95NBState::WAIT_QUERY_NET_REGISTRATION_RESPONSE;
+            break;
+        }
+        case BC95NBState::WAIT_QUERY_NET_REGISTRATION_RESPONSE:
+        {
+            m_stdModem.wait(300, STD_AtCmd::CEREG);
+            CEREG_t cereg;
+            m_stdModem.getCEREG(&cereg);
+            if (cereg.stat == 1)
+                m_state = BC95NBState::QUERY_NET_ATTACHMENT;
+            break;
+        }
+        case BC95NBState::QUERY_NET_ATTACHMENT:
+        {
+            m_stdModem.readCmd(STD_AtCmd::CGATT);
+            m_state = BC95NBState::WAIT_QUERY_NET_ATTACHMENT_RESPONSE;
+            break;
+        }
         }
         return m_state;
+    }
+
+    void getIMSI(char *_imsi)
+    {
+        strncpy(_imsi, m_imsi, STD_MODEM_MAX_IMSI_LENGTH);
     }
 
 private:
@@ -119,6 +162,55 @@ TEST_F(BC95NBTest, begin_ShouldReturnWAIT_QUERY_IMSI_RESPONSE)
     BC95NBState state = nb.begin();
     EXPECT_EQ(state, BC95NBState::WAIT_QUERY_IMSI_RESPONSE);
     EXPECT_STREQ(testSupport.getTxBuffer().c_str(), "AT+CIMI\r\n");
+}
+
+TEST_F(BC95NBTest, begin_ShouldReturnQUERY_NET_REGISTRATION)
+{
+    std::cout << "When receiving: "
+              << "\r\n460111174590523\r\n\r\nOK\r\n"
+              << std::endl;
+    testSupport.putRxBuffer("\r\n460111174590523\r\n\r\nOK\r\n");
+    Modem *modem = modemBuilder->buildModem();
+    BC95NB nb(modem);
+    nb.setState(BC95NBState::WAIT_QUERY_IMSI_RESPONSE);
+    BC95NBState state = nb.begin();
+    EXPECT_EQ(state, BC95NBState::QUERY_NET_REGISTRATION);
+    char imsi[STD_MODEM_MAX_IMSI_LENGTH];
+    nb.getIMSI(imsi);
+    EXPECT_STREQ("460111174590523", imsi);
+}
+
+TEST_F(BC95NBTest, begin_ShouldReturnWAIT_QUERY_NET_REGISTRATION_RESPONSE)
+{
+    Modem *modem = modemBuilder->buildModem();
+    BC95NB nb(modem);
+    nb.setState(BC95NBState::QUERY_NET_REGISTRATION);
+    BC95NBState state = nb.begin();
+    EXPECT_EQ(state, BC95NBState::WAIT_QUERY_NET_REGISTRATION_RESPONSE);
+    EXPECT_STREQ(testSupport.getTxBuffer().c_str(), "AT+CEREG?\r\n");
+}
+
+TEST_F(BC95NBTest, begin_ShouldReturnQUERY_NET_ATTACHMENT)
+{
+    std::cout << "When receiving: "
+              << "\r\n+CEREG:0,1\r\n\r\nOK\r\n"
+              << std::endl;
+    testSupport.putRxBuffer("\r\n+CEREG:0,1\r\n\r\nOK\r\n");
+    Modem *modem = modemBuilder->buildModem();
+    BC95NB nb(modem);
+    nb.setState(BC95NBState::WAIT_QUERY_NET_REGISTRATION_RESPONSE);
+    BC95NBState state = nb.begin();
+    EXPECT_EQ(state, BC95NBState::QUERY_NET_ATTACHMENT);
+}
+
+TEST_F(BC95NBTest, begin_ShouldReturnWAIT_QUERY_NET_ATTACHMENT_RESPONSE)
+{
+    Modem *modem = modemBuilder->buildModem();
+    BC95NB nb(modem);
+    nb.setState(BC95NBState::QUERY_NET_ATTACHMENT);
+    BC95NBState state = nb.begin();
+    EXPECT_EQ(state, BC95NBState::WAIT_QUERY_NET_ATTACHMENT_RESPONSE);
+    EXPECT_STREQ(testSupport.getTxBuffer().c_str(), "AT+CGATT?\r\n");
 }
 
 int main(int argc, char **argv)
