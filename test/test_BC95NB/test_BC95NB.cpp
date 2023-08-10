09b05831ab74fc8102ca914ecffc9dd9a18e30ef
Aung Khant Maw <aungkhantmaw64@gmail.com>
2023-08-10 12:00:24 +0700

    [feat] add test for WAIT_QUERY_IMSI_RESPONSE state of BC95NB modem

--- test/test_BC95NB/test_BC95NB.cpp
+++ test/test_BC95NB/test_BC95NB.cpp
@@ -8,18 +8,27 @@ TestSupport testSupport;
 enum class BC95NBState
 {
     QUERY_RF_FUNC,
-    WAIT_QUERY_RF_FUNC_RESPONSE
+    WAIT_QUERY_RF_FUNC_RESPONSE,
+    QUERY_IMSI,
+    WAIT_QUERY_IMSI_RESPONSE
 };
 
 class BC95NB
 {
 public:
-    BC95NB(Modem &modem)
-        : m_state(BC95NBState::QUERY_RF_FUNC){};
+    BC95NB(Modem *modem)
+        : m_state(BC95NBState::QUERY_RF_FUNC)
+    {
+        m_stdModem.attach(modem);
+    };
     ~BC95NB(){};
     BC95NBState getState()
     {
         return m_state;
+    }
+    void setState(BC95NBState _state)
+    {
+        m_state = _state;
     }
     BC95NBState begin()
     {
@@ -27,11 +36,23 @@ public:
         {
         case BC95NBState::QUERY_RF_FUNC:
         {
-
+            m_stdModem.readCmd(STD_AtCmd::CFUN);
+            m_state = BC95NBState::WAIT_QUERY_RF_FUNC_RESPONSE;
             break;
         }
         case BC95NBState::WAIT_QUERY_RF_FUNC_RESPONSE:
         {
+            m_stdModem.wait(300, STD_AtCmd::CFUN);
+            CFUN_t cfun;
+            m_stdModem.getCFUN(&cfun);
+            if (cfun.fun == 1)
+                m_state = BC95NBState::QUERY_IMSI;
+            break;
+        }
+        case BC95NBState::QUERY_IMSI:
+        {
+            m_stdModem.readCmd(STD_AtCmd::CIMI);
+            m_state = BC95NBState::WAIT_QUERY_IMSI_RESPONSE;
             break;
         }
         }
@@ -40,6 +61,9 @@ public:
 
 private:
     BC95NBState m_state;
+    StdModem m_stdModem;
+
+    char m_imsi[STD_MODEM_MAX_IMSI_LENGTH];
 };
 
 class BC95NBTest : public ::testing::Test
@@ -58,20 +82,43 @@ protected:
     }
 };
 
-TEST_F(BC95NBTest, beginsWithQueryRadioFunctionality)
+TEST_F(BC95NBTest, begin_ShouldReturnQUERY_RF_FUNC)
 {
     Modem *modem = modemBuilder->buildModem();
-    BC95NB nb(*modem);
+    BC95NB nb(modem);
     EXPECT_EQ(nb.getState(), BC95NBState::QUERY_RF_FUNC);
 }
 
-TEST_F(BC95NBTest, entersWaitForQueryRadioFunctionalityResponse)
+TEST_F(BC95NBTest, begin_ShouldReturnWAIT_QUERY_RF_FUNC_RESPONSE)
 {
     Modem *modem = modemBuilder->buildModem();
-    BC95NB nb(*modem);
+    BC95NB nb(modem);
     BC95NBState state = nb.begin();
     EXPECT_EQ(state, BC95NBState::WAIT_QUERY_RF_FUNC_RESPONSE);
     EXPECT_STREQ(testSupport.getTxBuffer().c_str(), "AT+CFUN?\r\n");
+}
+
+TEST_F(BC95NBTest, begin_ShouldReturnQUERY_IMSI)
+{
+    std::cout << "When receiving: "
+              << "\r\n+CFUN:1\r\n\r\nOK\r\n"
+              << std::endl;
+    Modem *modem = modemBuilder->buildModem();
+    testSupport.putRxBuffer("\r\n+CFUN:1\r\nOK\r\n");
+    BC95NB nb(modem);
+    nb.setState(BC95NBState::WAIT_QUERY_RF_FUNC_RESPONSE);
+    BC95NBState state = nb.begin();
+    EXPECT_EQ(state, BC95NBState::QUERY_IMSI);
+}
+
+TEST_F(BC95NBTest, begin_ShouldReturnWAIT_QUERY_IMSI_RESPONSE)
+{
+    Modem *modem = modemBuilder->buildModem();
+    BC95NB nb(modem);
+    nb.setState(BC95NBState::QUERY_IMSI);
+    BC95NBState state = nb.begin();
+    EXPECT_EQ(state, BC95NBState::WAIT_QUERY_IMSI_RESPONSE);
+    EXPECT_STREQ(testSupport.getTxBuffer().c_str(), "AT+CIMI\r\n");
 }
 
 int main(int argc, char **argv)
