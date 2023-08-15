3c40460911e54c4d10cc41ccc65f9c0b2f9daaa7
Aung Khant Maw <aungkhantmaw64@gmail.com>
2023-08-15 15:57:10 +0700

    [feat] add debug stream in Modem constructor to see AT responses during dev

--- examples/SendAtCommand/SendAtCommand.ino
+++ examples/SendAtCommand/SendAtCommand.ino
@@ -3,7 +3,9 @@
 
 const uint8_t resetPin = 4;
 const bool activeHigh = true;
-Modem modem(Serial2, resetPin, activeHigh);
+Modem modem(Serial2, resetPin, activeHigh, &Serial);
+BC95NB nb(&modem);
+
 String response;
 
 void setup()
@@ -12,18 +14,17 @@ void setup()
     Serial2.begin(9600);
     Serial.println("Triggered hard reset.");
     modem.reset();
-    modem.waitForResponse(5000, &response);
-    Serial.println(response);
+    modem.waitForResponse(5000);
     modem.send("AT\r\n");
-    modem.waitForResponse(300, &response);
-    Serial.println(response);
+    modem.waitForResponse(300);
     modem.send("AT+CFUN?\r\n");
-    modem.waitForResponse(300, &response);
-    Serial.println(response);
+    modem.waitForResponse(300);
     modem.send("AT+CGMR\r\n");
-    modem.waitForResponse(1000, &response);
-    Serial.println(response);
-    Serial.println("Done");
+    modem.waitForResponse(1000);
+    modem.send("AT+NCONFIG=AUTOCONNECT,TRUE\r\n");
+    modem.waitForResponse(2000);
+    modem.send("AT+NRB");
+    modem.waitForResponse(5000);
     while (true)
         ;
 }
