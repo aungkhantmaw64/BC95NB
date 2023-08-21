dea373c9b916fc4c7288b06ab65f0d228568907c
Aung Khant Maw <aungkhantmaw64@gmail.com>
2023-08-21 16:54:29 +0700

    [doc] add doxygen boilerplate comment documentation

--- src/Modem.h
+++ src/Modem.h
@@ -6,6 +6,9 @@
 #define MODEM_CMD_MAX_SIZE 100
 #define MODEM_MAX_RESPONSE_HANDLERS 4
 
+/**
+ * @brief Used to indicate the status of the AT command response
+ */
 enum class ResponseCode
 {
     EMPTY,
@@ -14,18 +17,56 @@ enum class ResponseCode
     CME_ERROR
 };
 
+/**
+ * @brief Modem Unsolicited Response Handler
+ *
+ */
 class ModemUrcHandler
 {
 public:
     virtual void onReceive(String &_response) = 0;
 };
+
+/**
+ * @brief  Class to send/receive raw AT commands/responses to/from the modem
+ *
+ */
 class Modem
 {
 public:
+    /**
+     * @brief Construct a new Modem object
+     *
+     * @param _stream Stream object to send/receive data to/from the modem
+     * @param _resetPin Reset pin for rebooting the modem
+     * @param _activeHigh Reset circuit type
+     * @param _debugStream Debug stream
+     */
     Modem(Stream &_stream, uint8_t _resetPin, bool _activeHigh, Stream *_debugStream = nullptr);
+    /**
+     * @brief reset the modem
+     *
+     */
     void reset();
+    /**
+     * @brief Sends an AT command to the modem. (e.g. "AT+CMD\r\n")
+     *
+     * @param _cmd Command to send
+     */
     void send(const char *_cmd);
+    /**
+     * @brief Wait for the modem to respond back to the command
+     * @param _timeoutMs timeout in milliseconds
+     * @param _response Response
+     * @return ResponseCode
+     */
     ResponseCode waitForResponse(uint32_t _timeoutMs, String *_response = nullptr);
+    /**
+     * @brief Add a urc handler for the modem.
+     *
+     * @param _handler Urc Handler
+     * @return int returns negative value if the max handler is reached.
+     */
     int addResponseHandler(ModemUrcHandler *_handler);
 
 private:
